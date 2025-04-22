58ee0701c629e0e82e328ef7dfb4c977aba9fb96
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-04-22 22:44:42 +0900

    eureka!

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -23,7 +23,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!!!@123777";
+        return "I'm alive!!!";
     }
 
 //    @GetMapping("/plus")
