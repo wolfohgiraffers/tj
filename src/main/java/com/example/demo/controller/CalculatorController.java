4fa27867b4195226663b445bcf8a28352a46f041
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-04-23 13:54:42 +0900

    webhook reset

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -23,7 +23,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!!!60초";
+        return "I'm alive!!!60초!";
     }
 
 //    @GetMapping("/plus")
