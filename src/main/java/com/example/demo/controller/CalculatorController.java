cba4d52a00d6f2d2c49584487476cf0e7b396f30
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-04-23 16:20:21 +0900

    finally

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -23,7 +23,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!!!only jen!!";
+        return "I'm alive!!! finally!";
     }
 
 //    @GetMapping("/plus")
