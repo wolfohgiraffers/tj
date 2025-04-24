abcbc3553dc04f3de67fbe9c4615cdc96f043c4d
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-04-24 16:42:08 +0900

    plus cd

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -23,7 +23,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!!! argo!!!";
+        return "I'm alive!!! argocd!!!";
     }
 
 //    @GetMapping("/plus")
